40d715035606cf3585785448deabfb8fb1b3a0c7
ManyCoding <vamukhs@gmail.com>
2015-02-16 19:36:55 -0800

    Updated BracketsValidator

--- MuchImproving/src/tasks_interviews/BracketsValidator.java
+++ MuchImproving/src/tasks_interviews/BracketsValidator.java
@@ -1,46 +1,93 @@
 package tasks_interviews;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class BracketsValidator {
 	// Exercise 1: Write a program that validates whether all of the braces,
-	// brackets,
-	// and parentheses are closed and properly nested.
-	// [{}] - True
-	// [{(})] - False
-	// [(] - False
+	// brackets, and parentheses are closed and properly nested.
+	// [{}] - true
+	// [{(})] - false
+	// [(] - false
+	// a(b) - true
+	// [(] - false
+	// }{ - false
+	// g([{}-[]]{e}) - true
+	// "" - true
 
 	private final static String openingBrackets = "([{<";
 	private final static String closingBrackets = ")]}>";
 
 	public static boolean checkBrackets(String input) {
-		Stack<Character> bracketsBuffer = new Stack<Character>();
+		Stack<Character> openedBrackets = new Stack<Character>();
+		
 		char[] inputs = input.toCharArray();
+		int x = 0;
 		for (char c : inputs) {
 			if (openingBrackets.indexOf(c) != -1) {
-				bracketsBuffer.push(c);
+				openedBrackets.push(c);
 			} else if (closingBrackets.indexOf(c) != -1) {
-				if (bracketsBuffer.isEmpty())
+				if (openedBrackets.isEmpty())
 					return false;
-				if (Math.abs(bracketsBuffer.pop() - c) > 2)
+				if (Math.abs(openedBrackets.pop() - c) > 2)
 					return false;
 			}
+
+			x++;
+			if (openedBrackets.size() > inputs.length - x)
+				return false;
 		}
 
-		return bracketsBuffer.isEmpty();
+		return openedBrackets.isEmpty();
 	}
 
 	public static void main(String[] args) {
-		assert checkBrackets("[{}]") == true;
+		assert checkBrackets("abcdeq((){})") == true;
+		assert checkBrackets("g([{}-[]]{y})") == true;
+		assert checkBrackets("[(abc]") == false;
+		assert checkBrackets("((())())()") == true;
 		assert checkBrackets("[{(})]") == false;
 		assert checkBrackets("[(]") == false;
 		assert checkBrackets("") == true;
-		assert checkBrackets("((((((((") == false;
+		assert checkBrackets("(((") == false;
 		assert checkBrackets("}}}}}}") == false;
 		assert checkBrackets("{}") == true;
 		assert checkBrackets("}{") == false;
 		assert checkBrackets(")") == false;
 		assert checkBrackets("(") == false;
 	}
-
 }
+
+// - Assume most recent language standard are available (ES5, SE8, C11,
+// C++11...).
+// - If missing more requirements details, just make reasonable assumptions of
+// your own.
+// - Solution must be simple and compact.
+// No defensive coding, no comments, no unrequested features.
+// Only one file 10-20 lines of code//
+
+// public static boolean check(String input) {
+// private String openingBrackets = “([{”;
+// String closingBrackets = “)]}”;
+// Stack<Character> bracketsBuffer = new Stack<Character>();
+// char[] inputs = input.toCharArray();
+// int x = 0;
+// for (char i : inputs) {
+// x++;
+// if (bracketsBuffer.size() > inputs.length - x)
+// return false;
+//
+// if (openingBrackets.indexOf(i) != -1){
+// bracketsBuffer.push(i);
+// }
+// else if (closingBrackets.charAt(i) != -1){
+// if (bracketsBuffer.isEmpty())
+// return false;
+// if (Math.abs(bracketsBuffer.pop() - i) > 2)
+// return false;
+// }
+// }
+//
+// return bracketsBuffer.isEmpty();
+// }
+
