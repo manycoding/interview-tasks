066087523296029eeade4035e7758dcd9890ccca
ManyCoding <vamukhs@gmail.com>
2015-02-16 19:37:51 -0800

    Cleaned BracketsValidator

--- MuchImproving/src/tasks_interviews/BracketsValidator.java
+++ MuchImproving/src/tasks_interviews/BracketsValidator.java
@@ -1,6 +1,5 @@
 package tasks_interviews;
 
-import java.util.HashMap;
 import java.util.Stack;
 
 public class BracketsValidator {
@@ -57,37 +56,3 @@ public class BracketsValidator {
 		assert checkBrackets("(") == false;
 	}
 }
-
-// - Assume most recent language standard are available (ES5, SE8, C11,
-// C++11...).
-// - If missing more requirements details, just make reasonable assumptions of
-// your own.
-// - Solution must be simple and compact.
-// No defensive coding, no comments, no unrequested features.
-// Only one file 10-20 lines of code//
-
-// public static boolean check(String input) {
-// private String openingBrackets = “([{”;
-// String closingBrackets = “)]}”;
-// Stack<Character> bracketsBuffer = new Stack<Character>();
-// char[] inputs = input.toCharArray();
-// int x = 0;
-// for (char i : inputs) {
-// x++;
-// if (bracketsBuffer.size() > inputs.length - x)
-// return false;
-//
-// if (openingBrackets.indexOf(i) != -1){
-// bracketsBuffer.push(i);
-// }
-// else if (closingBrackets.charAt(i) != -1){
-// if (bracketsBuffer.isEmpty())
-// return false;
-// if (Math.abs(bracketsBuffer.pop() - i) > 2)
-// return false;
-// }
-// }
-//
-// return bracketsBuffer.isEmpty();
-// }
-
