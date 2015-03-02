51a7737e227bc4dc8dc61c606f82028ff4aa710e
ManyCoding <vamukhs@gmail.com>
2015-03-02 15:49:33 -0800

    Cleaned and refactored

--- MuchImproving/src/tasks_arrays_Strings/ReplaceSpaces.java
+++ MuchImproving/src/tasks_arrays_Strings/ReplaceSpaces.java
@@ -3,28 +3,27 @@ package tasks_arrays_Strings;
 public abstract class ReplaceSpaces {
 	public static char[] replaceSpaces(char[] str, int length) {
 		int spaceCount = 0;
-		int newLength;
+		int newIndex;
 
 		for (int i = 0; i < length; i++)
 			if (str[i] == ' ')
 				spaceCount++;
 
-		newLength = length + spaceCount * 2;
+		newIndex = length + spaceCount * 2;
 		// str[newLength] = '\0';
 		for (int i = length - 1; i >= 0; i--) {
 
 			if (str[i] == ' ') {
-				str[newLength - 1] = '0';
-				str[newLength - 2] = '2';
-				str[newLength - 3] = '%';
-				newLength -= 3;
-				
+				str[newIndex - 1] = '0';
+				str[newIndex - 2] = '2';
+				str[newIndex - 3] = '%';
+				newIndex -= 3;
+
 				if (--spaceCount == 0)
 					break;
-			} else if (spaceCount > 0) {
-				System.out.println(i);
-				str[newLength - 1] = str[i];
-				newLength--;
+			} else {
+				str[newIndex - 1] = str[i];
+				newIndex--;
 			}
 		}
 		return str;
@@ -36,10 +35,7 @@ public abstract class ReplaceSpaces {
 }
 
 // 1.4 Write a method to replace all spaces in a string with '%20'. You may
-// assume that the
-// string has sufficient space at the end of the string to hold the additional
-// characters,
-// and that you are given the "true" length of the string. (Note: if
-// implementing in Java,
-// please use a character array so that you can perform this operation in
-// place.)
+// assume that the string has sufficient space at the end of the string to
+// hold the additional characters, and that you are given the "true" length
+// of the string. (Note: if implementing in Java, please use a character
+// array so that you can perform this operation in place.)
